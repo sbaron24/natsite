87fdc3960e556dfe95264fb0eb14810c9d83812e
Sean Baron <seanbaron27@gmail.com>
2020-06-27 18:30:34 -0400

    White aspirational text over image on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,21 @@ import Layout from "../components/layout"
 import Boat from "../images/boat.jpg"
 
 export default function Home() {
+
+  const quoteStyle = {
+    color: `white`,
+    position: `absolute`,
+    width: 300,
+    top: `8%`,
+    right: `5%`
+  }
+
   return (
     <Layout>
+      <div style={{position: `relative`}}>
       <img src={Boat} alt="" style={{border: "1px solid lightblue"}}></img>
+      <p style={quoteStyle}>Inspirational aspirational science-y words from a researcher out to make the oceans and this planet healthier for all living species.</p>
+      </div>
     </ Layout>
   )
 }
